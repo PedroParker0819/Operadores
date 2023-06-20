abd9ab4ad36afb99697e5e81464ea6c67a5ebd7c
Mono Gótica <113735545+PedroParker0819@users.noreply.github.com>
2023-06-20 19:57:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+primeiro_nome = "Pedro"
+ultimo_nome = "Parker"
+nome_completo = first_name + " " + last_name
+print(nome_completo)
+
+primeiro_nome = "Peter"
+ultimo_nome = "Parker"
+idade = 20
+altura = 1.80
+
+print(primeiro_nome + " " + ultimo_nome + " é " + str(idade) + " anos de idade" + str(altura) + " metros de altura.")
+
+const num1 = 10;
+const num2 = 20;
+
+console.log(`A soma de ${num1} e ${num2} é ${num1 + num2}`);
+console.log(`A diferença de ${num1} e ${num2} é ${num1 - num2}`);
+console.log(`O produto de ${num1} e ${num2} é ${num1 * num2}`);
+console.log(`O quociente de ${num1} e ${num2} é ${num1 / num2}`);
+
+const pi = 3.14;
+const radius = 5;
+
+const area = pi * radius * radius;
+
+console.log(`A área de um círculo com raio ${radius} é ${area}`);
